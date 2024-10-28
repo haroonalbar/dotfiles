7c056411c9583cddbd53193f712e12e67cc7ddc4
Haroon Albar <haroonalbarx@gmail.com>
2024-10-28 12:11:28 +0530

    remove transparancy and spaces

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -7,12 +7,12 @@ return {
       vim.g.everforest_background = "hard"
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-      vim.g.everforest_enable_italic = true
-      vim.g.everforest_colors_override = {
-        bg_dim = "#1c1c1c", -- sidebars
-        bg0 = "#1c1c1c", -- bg
-        bg2 = "#1c1c1c", -- terminal
-      }
+      -- vim.g.everforest_enable_italic = true
+      -- vim.g.everforest_colors_override = {
+      --   bg_dim = "#1c1c1c", -- sidebars
+      --   bg0 = "#1c1c1c", -- bg
+      --   bg2 = "#1c1c1c", -- terminal
+      -- }
       vim.cmd("colorscheme everforest")
     end,
   },
